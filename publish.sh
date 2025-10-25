a5f126377bc4efbb542bc057d3a4f3a42da4eae2
Gregory Vincic <gregoryvse@gmail.com>
2025-10-25 08:49:21 +0200

    Hide header and unreleased

--- publish.sh
+++ publish.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 [[ -z $PREV ]] && echo "set PREV" && exit 1
-[[ -z $TAG ]] && export TAG=main
 
-export DEST=docs/$TAG
+export REL=$1
+[[ -z $REL ]] && export TAG=main && export REL="unreleased"
+
+
+export DEST=docs/$REL
 export DATE=$(date -u +%F)
 export stp=stp
 
 #
 #  EIDT docs/$TAG/spec.txt and add header
 #
-echo "Version....: [${TAG#v}]
+echo "Version....: [${REL#v}]
 Obsoletes..: [${PREV#v}]
 Published..: $DATE
 Author.....: Gregory Vincic
 
-[${TAG#v}]: https://gregoryv.github.io/syhoc/$TAG
+[${TAG#v}]: https://gregoryv.github.io/syhoc/$REL
 [${PREV#v}]: https://gregoryv.github.io/syhoc/$PREV
 " > header.txt
 
@@ -29,10 +32,10 @@ $stp -i template.txt -o $DEST/index.html
 
 # move into docs so the diff titles are nice
 pushd docs
-python3 pydiff.py -file-a $PREV/spec.txt -file-b $TAG/spec.txt -o $TAG/diff_$PREV.html
+python3 pydiff.py -file-a $PREV/spec.txt -file-b $REL/spec.txt -o $REL/diff_$PREV.html
 popd
 
-rm spec.txt
+rm spec.txt header.txt
 
 
 #
